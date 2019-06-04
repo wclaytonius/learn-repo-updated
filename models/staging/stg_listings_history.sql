9878c8b50245235cdec0ba8f0627560bb8bf4b75
ericalouie <erica.louie94@gmail.com>
2019-06-04 15:40:54 -0400

    initial dbt commit

---
+++ models/staging/stg_listings_history.sql
@@ -0,0 +1,35 @@
+with source as (
+
+    select * from source_data.listing_history
+
+),
+
+renamed as (
+
+    select
+
+        listing_id,
+        nullif(replace(split_part(price, '$', 2), ',', ''), '') as price,
+        case
+            when available = 't'
+                then true
+            else false
+        end as available,
+        date as available_date
+
+    from source
+
+),
+
+with_id as (
+
+    select
+
+--use surrogate key from dbt-utils   ('listing_id', 'available_date') as id,
+        *
+
+    from renamed
+
+)
+
+select * from with_id
